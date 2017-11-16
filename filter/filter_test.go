8bcbbdf46fa6544eca2b907e73be41aaa63fbed4
Aldo Cortesi <aldo@corte.si>
2017-11-17 07:34:18 +1300

    Comprehensively reassess how we deal with file paths
    This results in a significant interface change, leander dependencies, and more
    consistent output.

--- filter/filter_test.go
+++ filter/filter_test.go
@@ -1,41 +1,11 @@
 package filter
 
 import (
-	"io/ioutil"
-	"os"
+	"fmt"
 	"path/filepath"
 	"reflect"
 	"testing"
 )
-
-// WithTempDir creates a temp directory, changes the current working directory
-// to it, and returns a function that can be called to clean up. Use it like
-// this:
-//      defer WithTempDir(t)()
-func WithTempDir(t *testing.T) func() {
-	cwd, err := os.Getwd()
-	if err != nil {
-		t.Fatalf("TempDir: %v", err)
-	}
-	tmpdir, err := ioutil.TempDir("", "")
-	if err != nil {
-		t.Fatalf("TempDir: %v", err)
-	}
-	err = os.Chdir(tmpdir)
-	if err != nil {
-		t.Fatalf("Chdir: %v", err)
-	}
-	return func() {
-		err := os.Chdir(cwd)
-		if err != nil {
-			t.Fatalf("Chdir: %v", err)
-		}
-		err = os.RemoveAll(tmpdir)
-		if err != nil {
-			t.Fatalf("Removing tmpdir: %s", err)
-		}
-	}
-}
 
 var filterFilesTests = []struct {
 	includes []string
@@ -65,16 +35,6 @@ var filterFilesTests = []struct {
 		[]string{"main.cpp", "main.h", "bar.py"},
 		false,
 	},
-	// Invalid patterns won't match anything. This would trigger a warning at
-	// runtime.
-	{
-		[]string{"*"},
-		[]string{"[["},
-		[]string{"main.cpp", "main.go", "main.h", "foo.go", "bar.py"},
-		[]string{"main.cpp", "main.go", "main.h", "foo.go", "bar.py"},
-		true,
-	},
-
 	{
 		[]string{"main.*"},
 		[]string{"*.cpp"},
@@ -100,139 +60,40 @@ var filterFilesTests = []struct {
 
 func TestFilterFiles(t *testing.T) {
 	for i, tt := range filterFilesTests {
-		result, err := Files(tt.files, tt.includes, tt.excludes)
-		if !tt.err && err != nil {
-			t.Errorf("Test %d: error %s", i, err)
-		}
-		if !reflect.DeepEqual(result, tt.expected) {
-			t.Errorf(
-				"Test %d (inc: %v, ex: %v), expected \"%v\" got \"%v\"",
-				i, tt.includes, tt.excludes, tt.expected, result,
-			)
-		}
+		t.Run(fmt.Sprintf("%.2d", i), func(t *testing.T) {
+			result, err := Files(tt.files, tt.includes, tt.excludes)
+			if !tt.err && err != nil {
+				t.Errorf("Test %d: error %s", i, err)
+			}
+			if !reflect.DeepEqual(result, tt.expected) {
+				t.Errorf(
+					"Test %d (inc: %v, ex: %v), expected \"%v\" got \"%v\"",
+					i, tt.includes, tt.excludes, tt.expected, result,
+				)
+			}
+		})
 	}
 }
 
-var BaseDirTests = []struct {
+var SplitPatternTests = []struct {
 	pattern  string
 	expected string
 }{
-	{"foo", "."},
-	{"test/foo", "test"},
-	{"test/foo*", "test"},
-	{"test/*.**", "test"},
-	{"**/*", "."},
-	{"foo*/bar", "."},
-	{"foo/**/bar", "foo"},
-	{"/voing/**", "/voing"},
+	{"foo", "foo"},
+	{"test/foo", "test/foo"},
+	{"test/foo*", "test/foo"},
+	{"test/*.**", "test/"},
+	{"**/*", ""},
+	{"foo*/bar", "foo"},
+	{"foo/**/bar", "foo/"},
+	{"/voing/**", "/voing/"},
 }
 
-func TestBaseDir(t *testing.T) {
-	for i, tt := range BaseDirTests {
-		ret := BaseDir(tt.pattern)
-		if filepath.ToSlash(ret) != filepath.ToSlash(tt.expected) {
-			t.Errorf("%d: %q - Expected %q, got %q", i, tt.pattern, tt.expected, ret)
-		}
-	}
-}
-
-var getBaseDirTests = []struct {
-	patterns []string
-	expected []string
-}{
-	{[]string{"foo"}, []string{"."}},
-	{[]string{"foo", "bar"}, []string{"."}},
-	{[]string{"foo", "bar", "/voing/**"}, []string{".", "/voing"}},
-	{[]string{"foo/**", "**"}, []string{"."}},
-	{[]string{"foo/**", "**", "/bar/**"}, []string{".", "/bar"}},
-}
-
-func TestGetBaseDirs(t *testing.T) {
-	for i, tt := range getBaseDirTests {
-		bp := []string{}
-		bp = AppendBaseDirs(bp, tt.patterns)
-		for i := range bp {
-			bp[i] = filepath.ToSlash(bp[i])
-		}
-		if !reflect.DeepEqual(bp, tt.expected) {
-			t.Errorf("%d: %#v - Expected %#v, got %#v", i, tt.patterns, tt.expected, bp)
-		}
-	}
-}
-
-var findTests = []struct {
-	include  []string
-	exclude  []string
-	expected []string
-}{
-	{
-		[]string{"**"},
-		[]string{},
-		[]string{"a/a.test1", "a/b.test2", "b/a.test1", "b/b.test2", "x", "x.test1"},
-	},
-	{
-		[]string{"**/*.test1"},
-		[]string{},
-		[]string{"a/a.test1", "b/a.test1", "x.test1"},
-	},
-	{
-		[]string{"**"},
-		[]string{"*.test1"},
-		[]string{"a/a.test1", "a/b.test2", "b/a.test1", "b/b.test2", "x"},
-	},
-	{
-		[]string{"**"},
-		[]string{"a"},
-		[]string{"b/a.test1", "b/b.test2", "x", "x.test1"},
-	},
-	{
-		[]string{"**"},
-		[]string{"a/"},
-		[]string{"b/a.test1", "b/b.test2", "x", "x.test1"},
-	},
-	{
-		[]string{"**"},
-		[]string{"**/*.test1", "**/*.test2"},
-		[]string{"x"},
-	},
-}
-
-func TestFind(t *testing.T) {
-	defer WithTempDir(t)()
-	paths := []string{
-		"a/a.test1",
-		"a/b.test2",
-		"b/a.test1",
-		"b/b.test2",
-		"x",
-		"x.test1",
-	}
-	for _, p := range paths {
-		dst := filepath.Join(".", p)
-		err := os.MkdirAll(filepath.Dir(dst), 0777)
-		if err != nil {
-			t.Fatalf("Error creating test dir: %v", err)
-		}
-		err = ioutil.WriteFile(dst, []byte("test"), 0777)
-		if err != nil {
-			t.Fatalf("Error writing test file: %v", err)
-		}
-	}
-
-	for i, tt := range findTests {
-		ret, err := Find(".", tt.include, tt.exclude)
-		if err != nil {
-			t.Fatal(err)
-		}
-		expected := tt.expected
-		for i := range ret {
-			ret[i] = filepath.ToSlash(ret[i])
-		}
-		if !reflect.DeepEqual(ret, expected) {
-			t.Errorf(
-				"%d: %#v, %#v - Expected\n%#v\ngot:\n%#v",
-				i, tt.include, tt.exclude, expected, ret,
-			)
+func TestSplitPattern(t *testing.T) {
+	for i, tt := range SplitPatternTests {
+		bdir, _ := SplitPattern(tt.pattern)
+		if filepath.ToSlash(bdir) != filepath.ToSlash(tt.expected) {
+			t.Errorf("%d: %q - Expected %q, got %q", i, tt.pattern, tt.expected, bdir)
 		}
 	}
 }
